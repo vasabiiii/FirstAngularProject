92b93c328c2b9f43e3e32e134e3effed9919d6d8
vasabiiii <126241531+vasabiiii@users.noreply.github.com>
2023-12-20 17:51:50 +0600

    Add files via upload

---
+++ src/app/yourFeed/yourFeed.module.ts
@@ -0,0 +1,29 @@
+import {NgModule} from '@angular/core'
+import {CommonModule} from '@angular/common'
+import {RouterModule} from '@angular/router'
+
+import {YourFeedComponent} from 'src/app/yourFeed/components/yourFeed/yourFeed.component'
+import {FeedModule} from 'src/app/shared/modules/feed/feed.module'
+import {BannerModule} from 'src/app/shared/modules/banner/banner.module'
+import {PopularTagsModule} from '../shared/modules/popularTags/popularTags.module'
+import {FeedTogglerModule} from 'src/app/shared/modules/feedToggler/feedToggler.module'
+
+const routes = [
+  {
+    path: 'feed',
+    component: YourFeedComponent
+  }
+]
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FeedModule,
+    BannerModule,
+    PopularTagsModule,
+    FeedTogglerModule
+  ],
+  declarations: [YourFeedComponent]
+})
+export class YourFeedModule {}
